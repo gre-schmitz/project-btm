0d02a34a2dda8ac9457d05e35c51de3764d9db45
Gregor Schmitz <gre.schmitz@gmx.de>
2024-03-29 16:19:24 +0000

    disclaimer

--- btm/api/Home.py
+++ btm/api/Home.py
@@ -15,3 +15,17 @@ st.markdown("# __Beat the Market__ #")
 
 st.markdown("[Go to computations](Computations)")
 st.markdown("[Find out about or methods](Background)")
+
+"### Disclaimer###"
+"""
+This application is intended for informational and educational purposes only. The fair values provided for the S&P 500 are based on certain algorithms and methodologies which may not accurately predict market movements or future stock prices. The information provided by this application should not be considered as financial advice or a recommendation to buy, sell, or hold any securities.
+"""
+"""
+Trading and investing in the stock market involves inherent risks, including the risk of loss of capital. The user of this application acknowledges and agrees that they are solely responsible for any investment decisions made based on the information provided by this application. The creator of this application shall not be held liable for any losses, damages, or legal consequences arising from the use of this application or reliance on its content.
+"""
+"""
+Users are advised to conduct their own research, seek professional advice, and exercise caution before making any investment decisions. Past performance is not indicative of future results, and the stock market is subject to various factors and uncertainties that may cause prices to fluctuate unpredictably.
+"""
+"""
+By using this application, the user agrees to release the creator from any and all liability arising from the use of the application or reliance on its content.
+"""
